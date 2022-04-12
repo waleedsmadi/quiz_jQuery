8756a6995d31494cb8e6203b9142d37729a0276a
waleed smadi <waleed0smadi@outlook.com>
2022-04-12 22:29:42 +0300

    finish result.js and result.html

---
+++ js/result.js
@@ -0,0 +1,9 @@
+// A custom result file has been created
+// for possible expansion of the result file
+
+
+
+// showing the result
+const theScore = localStorage.getItem("the_score");
+
+$("[data-result]").text(theScore);
